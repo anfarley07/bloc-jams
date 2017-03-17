926f884a16f32b5e12010838367bdb4d92b201d6
Amy Farley <amyfarleyseattle@gmail.com>
2017-03-17 14:33:42 -0700

    Correcting revealPoint logic

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
 var animatePoints = function() {
    var points = document.getElementsByClassName("point")
-   var revealPoints = function(points) {
+   var revealPoint = function(i) {
       points[i].style.opacity = 1;
       points[i].style.transform = "scaleX(1) translateY(0)";
       points[i].style.msTransform = "scaleX(1) translateY(0)";
@@ -8,8 +8,6 @@ var animatePoints = function() {
     };
     
   for (var i = 0; i < points.length; i++) {
-      revealPoints(points);
+      revealPoint(i);
   }  
 };
-
- 
