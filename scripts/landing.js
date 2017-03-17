098c5a7a952a952f0e847a09b63f4353b90f5117
Amy Farley <amyfarleyseattle@gmail.com>
2017-03-17 11:57:44 -0700

    Minor syntax correction to Assignment 8

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,11 +1,12 @@
 var animatePoints = function() {
    var points = document.getElementsByClassName("point")
    var revealPoints = function(points) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-      };
+      points[i].style.opacity = 1;
+      points[i].style.transform = "scaleX(1) translateY(0)";
+      points[i].style.msTransform = "scaleX(1) translateY(0)";
+      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+    
   for (var i = 0; i < points.length; i++) {
       revealPoints(points);
   }  
