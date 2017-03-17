4a6a707c2682e245d82b8ecde8ca4a52dc3711a3
Amy Farley <amyfarleyseattle@gmail.com>
2017-03-17 13:43:37 -0700

    Checkpoint event handling work

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,30 @@
-var animatePoints = function() {
-   var points = document.getElementsByClassName("point")
-   var revealPoints = function(points) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+var pointsArray = document.getElementsByClassName("point")
+
+var animatePoints = function(points) {
+   var revealPoints = function(index) {
+      pointsArray[i].style.opacity = 1;
+      pointsArray[i].style.transform = "scaleX(1) translateY(0)";
+      pointsArray[i].style.msTransform = "scaleX(1) translateY(0)";
+      pointsArray[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
     
-  for (var i = 0; i < points.length; i++) {
+  for (var i = 0; i < pointsArray.length; i++) {
       revealPoints(points);
   }  
 };
 
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
  
