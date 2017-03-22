9f44a8e7ea7d73ac736fc636018c810e36a3e894
Amy Farley <amyfarleyseattle@gmail.com>
2017-03-22 13:34:41 -0700

    attempt at callback assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
 var animatePoints = function() {
    var points = document.getElementsByClassName("point")
-   var revealPoints = function(points) {
+   var revealPoint = function(i) {
       points[i].style.opacity = 1;
       points[i].style.transform = "scaleX(1) translateY(0)";
       points[i].style.msTransform = "scaleX(1) translateY(0)";
@@ -8,8 +8,22 @@ var animatePoints = function() {
     };
     
   for (var i = 0; i < points.length; i++) {
-      revealPoints(points);
+      revealPoint(i);
   }  
 };
 
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(points);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(points);
+        }
+    });
+}
  
