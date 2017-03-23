3ac4c1303bb646087a588cd4d7042e9bb535fb43
Amy Farley <amyfarleyseattle@gmail.com>
2017-03-22 17:10:30 -0700

    Refactored callback and abstraction of point

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,14 @@
-var animatePoints = function() {
-   var points = document.getElementsByClassName("point")
-   var revealPoint = function(i) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+var points = document.getElementsByClassName("point")
+var animatePoints = function(points) {
+   var revealPoint = function(point) {
+       console.log(point);
+      point.style.opacity = 1;
+      point.style.transform = "scaleX(1) translateY(0)";
+      point.style.msTransform = "scaleX(1) translateY(0)";
+      point.style.WebkitTransform = "scaleX(1) translateY(0)";
     };
     
-  for (var i = 0; i < points.length; i++) {
-      revealPoint(i);
-  }  
+  forEach(points, revealPoint);
 };
 
 window.onload = function() {
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,17 +1,5 @@
-var points = document.getElementsByClassName("point")
-var forEach = function(i, loopCallback) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-};
-    
-var loopCallback = function(){
-    for (var i = 0; i < points.length; i++) {
-      forEach(i);
+var forEach = function(array, callback) {
+    for (var i = 0; i < array.length; i++){
+    callback(array[i]);
   }
-}; 
-
-
-
-
+};
