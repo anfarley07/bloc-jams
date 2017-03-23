3a8b6c7bdc3eec7298111cb9021cb0bbcf8b80a1
Amy Farley <amyfarleyseattle@gmail.com>
2017-03-23 16:49:53 -0700

    Corrected Checkpoint11 so setCurrentAlbum functions

--- scripts/album.js
+++ scripts/album.js
@@ -45,7 +45,7 @@ var setCurrentAlbum = function(album) {
     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
     var albumImage = document.getElementsByClassName('album-cover-art')[0];
     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    albumTitle.FirstChild.nodeValue = album.title;
+    albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
     albumImage.setAttribute('src', album.albumArtUrl);
