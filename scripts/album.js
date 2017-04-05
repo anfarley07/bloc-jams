6fd51d2cf43337e2565d32d58874fdf6c85c192a
Amy Farley <amyfarleyseattle@gmail.com>
2017-04-05 14:07:43 -0700

    Second attempt at chkpt13 - undefined error

--- scripts/album.js
+++ scripts/album.js
@@ -67,8 +67,8 @@ function findParentByClassName (element, targetClass) {
 };
 
 //This determienes whethether to display the play/pause button, or the song number.
-function getSongItem(element) {
-    switch(element){
+function getSongItem(targetElement) {
+    switch(targetElement){
         case 'album-song-button':
         case 'ion-play':
         case 'ion-pause':
@@ -87,15 +87,33 @@ function getSongItem(element) {
     }
 }
 
-var clickHandler = function(targetElement);
-var songItem = getSongItem(targetElement);
+//conditional that indicates when to change between play and pause
+var clickHanlder = function(targetElement) {
+    var songItem = getSongItem(targetElement); 
+    //assigns pause button template if there's not a currently playing song
+    if (currentlyPlayingSong === null) {
+        songItem.innerHTML = pauseButtonTemplate;
+        currentlyPlayingSong = songItem.getAttribute('data-song-number');
+    //changes currently playing song to null and removes pause if pause button is clicked
+    }  else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+          songItem.innerHTML = playButtonTemplate;
+          currentlyPlayingSong = null;
+    //in case a song is currently playing and another song is clicked then this assigns pause button to new song
+    } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+        var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+        currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+        songItem.innerHTML = pauseButtonTemplate;
+        currentlyPlayingSong = songItem.getAttribute('data-song-number');
+    }
+};
 
-var currentlyPlayingSong = null
+
 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>'
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var currentlyPlayingSong = null;
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
@@ -110,12 +128,10 @@ window.onload = function() {
         songRows[i].addEventListener('mouseleave', function(event) {
            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number'); 
         });
-    //Seems like something may be missing here. Perhaps an if statement? Goal is to have a pause button show after play button is clicked
+    //this calls clickhandler on click
     songRows[i].addEventListener('click', function(event) {
-        if (event.target.parentElement.className === 'album-view-song-item') {
-            event.target.parentElement.querySelector('.song-item-number').innerHTML = pauseButtonTemplate;
-     });
-    }
+        clickHanlder(event.target);
+    });
   }
 };
 
